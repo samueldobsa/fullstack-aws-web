459a4488f6f53414f03af16846a5e1a5015d0740
Samuel Dobsa <samuel.dobsa@amcef.sk>
2023-12-15 23:54:31 +0100

    Microservice with using AWS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
+import axios from "axios";
+
+const UserProfiles = () => {
+const [userProfiles, setUserProfiles] = useState ([]);
+
+const fetchUserProfiles = () => {
+    axios.get("http://localhost:8080/api/v1/user-profile").then(res => {
+        console.log(res);
+        const data = res.data;
+        setUserProfiles(res.data);
+    });
+    };
+
+    useEffect(() => {
+        fetchUserProfiles();
+    }, []);
+
+    return userProfiles.map((userProfile, index) => {
+        return (
+            <div key={index}>
+                <h1>{userProfile.userName}</h1>
+                <p>{userProfile.userProfileId}</p>
+            </div>
+    )
+    })
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">
+    <UserProfiles />
+  </div>;
 }
 
 export default App;
